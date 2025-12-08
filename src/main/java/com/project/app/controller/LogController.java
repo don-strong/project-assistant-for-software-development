a78514f3a1d696a82fcab8a99b12c86a664b10ec
Luci Crow <sethgriffithhotvedt@gmail.com>
2025-12-07 17:01:48 -0800

    Updated to use the backend with the fxml
    Now you can type into the text fields to edit the backend

--- src/main/java/com/project/app/controller/LogController.java
+++ src/main/java/com/project/app/controller/LogController.java
@@ -1,10 +1,39 @@
 package com.project.app.controller;
 
+import com.project.app.model.KnowBaseMain;
+import com.project.app.model.KnowDB;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class LogController {
     @FXML
+    private TextField idField;
+    @FXML
+    private TextField problemField;
+    @FXML
+    private TextField solutionField;
+    @FXML
+    private TextField keywordField;
+    @FXML
+    private TextField languageField;
+    @FXML
+    private TextField ideField;
+
+    
+
+    @FXML
     protected void handleSave(){
-        throw new UnsupportedOperationException("Not supported yet.");
+        KnowBaseMain knowBaseMain = new KnowBaseMain();
+        KnowDB knowDBEntry = new KnowDB(
+                idField.getText(),
+                keywordField.getText(),
+                languageField.getText(),
+                ideField.getText(),
+                problemField.getText(),
+                solutionField.getText()
+        );
+        knowBaseMain.insertKnowDB(knowDBEntry);
     }
+
 }
